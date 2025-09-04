c104184cbcb10a3b06c0796f4da3bafb39c58c46
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-09-04 19:48:57 +0200

    add summary from Mussa

--- summary/summary-basic.js
+++ summary/summary-basic.js
@@ -5,3 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 function sectionCheck(idsecTrue) {
     document.getElementById(idsecTrue).classList.add('active')
 }})
+
+function summaryToBoard() {
+    window.location.href = '../board/board.html';
+}

bf19a493b3199383b216e7d7040fd8c4f5854c86
Arnesto <arnestoarnesto94@gmail.com>
2025-09-25 22:48:11 +0200

    summary img problems solved ( Fred Object ) done by : Arnesto

--- summary/summary-basic.js
+++ summary/summary-basic.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', async () => {
      init();
     sectionCheck('summary')
+    toDoSummaryEventHandler()
+    doneSummaryEventHandler()
 
 function sectionCheck(idsecTrue) {
     document.getElementById(idsecTrue).classList.add('active')
@@ -8,4 +10,28 @@ function sectionCheck(idsecTrue) {
 
 function summaryToBoard() {
     window.location.href = '../board/board.html';
+}
+
+function toDoSummaryEventHandler() {
+    const box = document.querySelector('.box-upper-left')
+    box.addEventListener('mouseenter',()=>{
+        const imgBox = box.querySelector('img')
+        imgBox.setAttribute('src',"../img/icons/summary-pencil-hover.svg")
+    })
+        box.addEventListener('mouseleave',()=>{
+        const imgBox = box.querySelector('img')
+        imgBox.setAttribute('src',"../img/icons/summary-pencil-default.svg")
+    })
+}
+
+function doneSummaryEventHandler() {
+    const box = document.querySelector('.box-upper-right')
+    box.addEventListener('mouseenter',()=>{
+        const imgBox = box.querySelector('img')
+        imgBox.setAttribute('src',"../img/icons/summary-done-icon-hover.svg")
+    })
+        box.addEventListener('mouseleave',()=>{
+        const imgBox = box.querySelector('img')
+        imgBox.setAttribute('src',"../img/icons/summary-done-icon-default.svg")
+    })
 }

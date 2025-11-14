4cdf8c101e56d2a54499f7b9cb23a64771c00903
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-11-14 01:36:43 +0100

    add join-Logo and prevent Minimenü for Responsiv

--- Board/board-renderTemplates.js
+++ Board/board-renderTemplates.js
@@ -117,7 +117,9 @@ function renderTaskintoBoard(element) {
   }
   return `<div draggable="true" ondragstart="startDragging(${element['id']})" 
     id="TaskDiv-${element.id}" onclick="bigViewOfTask(${element.id}); renderContactForBigView(${element.id}); renderEditAndDeleteButton(${element.id})" class="TaskDiv">
-    <div id="taskType" class="${taskOption}">${element.taskType}</div>
+    <div class=" dOntShow" id="miniMenüResponsiv-${element.id}">Optionen in arbeit</div>
+    <button class="miniMenüButton" onclick="event.stopPropagation(); renderMiniMenü(${element.id})">MiniMenü</button>
+    <div  id="taskType" class="${taskOption}">${element.taskType}</div>
     <div class="taskTitle"><p>${element.title}</p></div>
     <div class="taskDescription"><p>${element.description}</p></div>
     <div class="subTasks">
@@ -139,6 +141,13 @@ function renderTaskintoBoard(element) {
         </div>
         <div></div>
         </div>`
+}
+
+function renderMiniMenü(id){
+  //  let mini = tasks.filter(tasks => tasks[1].id === id);
+  miniMenu = document.getElementById(`miniMenüResponsiv-${id}`)
+  miniMenu.classList.toggle('miniMenüResponsiv')
+  console.log('wird getriggert')
 }
 
 function renderBigViewHTML(elements, id) {
